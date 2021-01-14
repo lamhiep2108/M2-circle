ac64bb73026a3fdb4b3c6650ac6f1ec032549cde
Admin <hiepnothip@gmail.com>
2021-01-14 16:25:28 +0700

    'l

---
+++ src/Circle/Circle.java
@@ -0,0 +1,17 @@
+package Circle;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+     public Circle(double radius){
+         this.radius=radius;
+     }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+         return radius*radius * 3.14;
+    }
+}
+
---
+++ src/Circle/TestCircle.java
@@ -0,0 +1,9 @@
+package Circle;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle TestCircle1 =new Circle(5.0);
+        System.out.println(TestCircle1.getArea());
+    }
+
+}
